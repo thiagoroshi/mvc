328822bc5d9ec4b830a52dbaf3098b7371352117
thiagoroshi <mutenroshi@live.com>
2014-08-28 18:33:40 -0300

    finish
    agora so aplicar a teoria

--- app/core/app.php
+++ app/core/app.php
@@ -10,18 +10,18 @@ class App
 
 	public function __construct()
 	{
-		$url = $this->parseUrl(); 
+		$url = $this->parseUrl();
 
-		if(file_exists('../app/controller/' . $url[0] . '.php'))
+		if(file_exists('../app/controllers/' . $url[0] . '.php'))
 		{
 			$this->controller = $url[0];
 			unset($url[0]);
 		}
-		require_once '../app/controller/' . $this->controller . '.php';
+		require_once '../app/controllers/' . $this->controller . '.php';
 
 		$this->controller = new $this->controller;
 
-			
+
 		if(isset($url[1]))
 		{
 			if(method_exists($this->controller, $url[1]))
@@ -30,7 +30,7 @@ class App
 				unset($url[1]);
 			}
 		}
-		
+
 		$this->params = $url ? array_values($url) : [];
 
 		call_user_func_array([$this->controller, $this->method], $this->params);
--- app/core/controller.php
+++ app/core/controller.php
@@ -4,7 +4,12 @@ class Controller
 {
 	protected function model($model)
 	{
-		require_once '../app/model/' . $model . '.php';
+		require_once '../app/models/' . $model . '.php';
 		return new $model();
 	}
+
+    protected function view($view, $data = [])
+    {
+        require_once '../app/views/' . $view . '.php';
+    }
 }
